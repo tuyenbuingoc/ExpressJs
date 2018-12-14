37a13c8ec872bb46326f3a9c73ae03e7e4f59a0c
TuyenBui <buingoctuyen08@gmail.com>
2018-12-14 15:56:40 +0700

    Set view engine and user page

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+var users = [
+    {id: 1, name: 'Tuyen'},
+    {id: 2, name: 'Bui'}
+];
 app.get('/', function(req, res) {
-    res.send('<h1 style="color: blue">Hello<h1><a href="#" style="text-decoration:none">User</a>');
+    res.render('index', {
+        name: 'TuyenBui'
+    });
 });
+app.get('/user', function(req, res) {
+    res.render('user/index', {
+        users: users
+    });
+});
+app.get('/user/search', function(req, res) {
+    var q = req.query.q;
+    var matchedUsers = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q) !== -1;
+    })
+    res.render('user/index', {
+        users: matchedUsers
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
